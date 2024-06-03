1dc9d88e52ba1ebd7b03b1c87b225281b676ed60
yanlong wang <18621056089@163.com>
2024-06-03 16:14:50 +0200

    Last Modify For Part2 Of Container And Slurm

--- Job.sh
+++ Job.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 # Set job name
-#SBATCH --job-name=matrix_mult_job
+#SBATCH --job-name=matrix_mult_job       # Job name
 
 # Redirect output and error
-#SBATCH --output=matrix_mult_output.txt
-#SBATCH --error=matrix_mult_error.txt
+#SBATCH --output=matrix_mult_output.txt  # Output file
+#SBATCH --error=matrix_mult_error.txt    # Error file
 
 # Set Parameters
-#SBATCH --ntasks=4
-#SBATCH --time=01:00:00
+#SBATCH --time=0:10:00                   # Time Limit (hh:mm:ss)   
+#SBATCH --ntasks=2                       # Number of tasks
+#SBATCH --nodes=1                        # Number of nodes
+#SBATCH --partition=g100_all_serial      # Partition to submit job
 
-# Run the containerized application
-cd build
-singularity exec ../matrix_mult_1.0.sif cmake ..
-singularity exec ../matrix_mult_1.0.sif make
-singularity exec ../matrix_mult_1.0.sif ctest
+
+# Core Work Of This Job File: Do matrix multiplication by using MPI (2 tasks).
+singularity exec ~/seproject//matrix_mult.sif mpiexec -np 2 ~/seproject//main
+
+
+
+# -------------------------------------------------------------------------------------
+# Next work is do for fun to see if the container works fine with cmake build and test.
+# -------------------------------------------------------------------------------------
+
+# Navigate to the build directory
+cd ~/seproject/build
+
+# Run CMake to test if the previous test cases will work rightly with container
+singularity exec ~/seproject//matrix_mult.sif cmake .. || { echo 'CMake configuration failed' ; exit 1; }
+singularity exec ~/seproject//matrix_mult.sif make || { echo 'Build failed' ; exit 1; }
+singularity exec ~/seproject//matrix_mult.sif ctest || { echo 'Tests failed' ; exit 1; }
